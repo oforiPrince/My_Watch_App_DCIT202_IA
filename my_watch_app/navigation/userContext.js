64bc781298ce2728ff008777d28013f9c02e7d34
oforiPrince <Pofori034@st.ug.edu.gh>
2021-11-17 20:52:43 +0000

    updated

--- my_watch_app/navigation/userContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const UserContext =React.createContext({
-    userData: null
-})
-
-export default UserContext;
---
+++ my_watch_app/screen/settings.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const settings = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default settings
+
+const styles = StyleSheet.create({})
